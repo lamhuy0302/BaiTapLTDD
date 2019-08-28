05b7cd32936b2ee9086ee5d10a7b025eeb663a02
unknown <hacklachek113@gmail.com>
2019-08-29 01:25:21 +0700

    final

---
+++ Tuan2_1/app/src/main/java/com/example/student/bai1/ActivityBai5.java
@@ -0,0 +1,13 @@
+package com.example.student.bai1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityBai5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai5);
+    }
+}
